d57ef2a0cb56b30f90cfad34213e92fa52cb87f8
Bálint <47783658+Roharx@users.noreply.github.com>
2023-02-20 11:24:43 +0100

    Added be and interface for DAL

--- src/be/Book.java
+++ src/be/Book.java
@@ -1,4 +1,74 @@
 package be;
 
+import java.util.Date;
+
 public class Book {
+
+    private int isbn;
+    private String title;
+    private boolean rented;
+    private boolean ebook;
+    private int rating;
+    private Date releaseDate;
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+
+    public void setRented(boolean rented) {
+        this.rented = rented;
+    }
+
+    public boolean isEbook() {
+        return ebook;
+    }
+
+    public void setEbook(boolean ebook) {
+        this.ebook = ebook;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn=" + isbn +
+                ", title='" + title + '\'' +
+                ", rented=" + rented +
+                ", ebook=" + ebook +
+                ", rating=" + rating +
+                ", releaseDate=" + releaseDate +
+                '}';
+    }
 }
--- src/dal/IDataAccess.java
+++ src/dal/IDataAccess.java
@@ -1,4 +1,21 @@
 package dal;
 
+import be.Book;
+import be.Category;
+
+import java.util.List;
+
 public interface IDataAccess {
+
+    void addBook(Book book);
+    String getAllBooks();
+    void editBook(int bookID, Book book);
+    void deleteBook(int bookID);
+
+
+    String getAllAuthors();
+    Book getBookByAuthor(int authorID);
+    List<Book> getBookByCategory(Category category);
+    List<Book> getAllBooksByAuthor(int authorID);
+
 }
