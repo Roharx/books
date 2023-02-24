70462d0ef92ef9c184411ce9cc1bccd90b11026e
Bálint <47783658+Roharx@users.noreply.github.com>
2023-02-24 19:16:52 +0100

    Finished base design in css and fxml

--- src/pl/MainController.java
+++ src/pl/MainController.java
@@ -2,6 +2,10 @@ package pl;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -11,21 +15,37 @@ public class MainController implements Initializable {
 
     @FXML
     public AnchorPane anpMain,
-    anpUserControls,
-    anpContent;
+            anpUserControls,
+            anpContent;
+    @FXML
+    public TextArea txaNote;
+    @FXML
+    public TextField txfSearch;
+    @FXML
+    public Button btnCancelNote,
+            btnSaveNote,
+            btnSearch,
+            btnHistory,
+            btnMenu;
 
-    MainModel mainModel;
+    @FXML
+    public TableView tbvBooks,
+    tbvCategories;
 
-    public MainController(){
+
+    private MainModel mainModel;
+
+    public MainController() {
         mainModel = new MainModel();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         displayHome();
     }
 
-    private void displayHome(){
+    private void displayHome() {
 
     }
 
